44e300bd555e9c6023322da5ee5a6a376dc21cda
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-04-12 11:47:27 +0530

    added the bublle sort algorithm

---
+++ bubble sort algorithm/Cpp.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+void bubbleSort(int arr[],int n){
+    int i,j;
+    bool swapped = false;
+    for(i=0;i<n-1;i++){
+        swapped = false;
+        for(j=0;j<n-i-1;j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                swapped = true;
+            }
+        }
+        if(!swapped) break;
+    }
+}
