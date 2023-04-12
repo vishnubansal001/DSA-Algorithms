6c5e1879ff72e8638aaa703722389d47634e0fdd
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-04-12 11:44:06 +0530

    selection sort algo added

---
+++ selection sort algorithm/Cpp.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+void selectionSort(int arr[],int n){
+    int i,j,min_idx;
+    for(i=0;i<n-1;i++){
+        min_idx = i;
+        for(j=i+1;j<n;j++){
+            if(arr[j]<arr[min_idx]){
+                min_idx = j;
+            }
+        }
+        if(min_idx!=i) swap(arr[min_idx],arr[i]);
+    }
+}
