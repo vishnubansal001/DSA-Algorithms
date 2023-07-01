95abaf1689d228b44da869ac2153d411cb28f36e
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-07-01 12:01:47 +0530

    new:added the java code for the inserion sort algorithm

---
+++ insertion Sort Algorithm/Java.java
@@ -0,0 +1,35 @@
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Array before sorting:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("Array after sorting:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
