2d40a465c64260ba6eff52dd1daa2b63041936f9
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-07-01 12:10:18 +0530

    new:added the java code for the selection sort algorithm

---
+++ selection sort algorithm/Java.java
@@ -0,0 +1,37 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            if (min_idx != i) {
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Array before sorting:");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("Array after sorting:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
