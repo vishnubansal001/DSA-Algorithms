9aa352be594a0f11b4ead1492fc5cecb1a5d7f1a
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-07-04 22:48:16 +0530

    update:added the java code for the linear search

---
+++ linear search algorithm/Java.java
@@ -0,0 +1,13 @@
+public class LinearSearch {
+    public static int search(int[] arr, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int index = search(arr, target);
+    }
+}
